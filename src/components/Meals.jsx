50b3207175b8e90fb819ab2da7422c6715e7c776
Alex Willis <willisalex@hotmail.co.uk>
2025-12-16 10:03:41 +0000

    Added Meals component

---
+++ src/components/Meals.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+export default function Meals() {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch("https://xgrdj5-3000.csb.app/meals");
+
+      if (!response.ok) {
+        // ...
+      }
+
+      const meals = await response.json();
+      setLoadedMeals(meals);
+    }
+
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <li key={meal.id}>{meal.name}</li>
+      ))}
+    </ul>
+  );
+}
