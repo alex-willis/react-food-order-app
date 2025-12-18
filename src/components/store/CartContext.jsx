c298085d16eb1893d2bbf551f58c84a8c23e79b8
Alex Willis <willisalex@hotmail.co.uk>
2025-12-18 17:54:11 +0000

    Added CartItem component

--- src/components/store/CartContext.jsx
+++ src/components/store/CartContext.jsx
@@ -9,7 +9,7 @@ const CartContext = createContext({
 function cartReducer(state, action) {
   if (action.type === "ADD_ITEM") {
     const existingCartItemIndex = state.items.findIndex(
-      (item) => item.id === action.id
+      (item) => item.id === action.item.id
     );
 
     const updatedItems = [...state.items];
@@ -31,7 +31,7 @@ function cartReducer(state, action) {
 
   if (action.type === "REMOVE_ITEM") {
     const existingCartItemIndex = state.items.findIndex(
-      (item) => item.id === action.item.id
+      (item) => item.id === action.id
     );
     const existingCartItem = state.items[existingCartItemIndex];
 
