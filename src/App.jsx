5634e4135e0c3e251744a6e7b5eed1803d73112f
Alex Willis <willisalex@hotmail.co.uk>
2025-12-18 17:06:45 +0000

    Used Cart Context to add items to cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useEffect } from "react";
 import Header from "./components/Header";
 import Meals from "./components/Meals";
+import { CartContextProvider } from "./components/store/CartContext";
 
 function App() {
   return (
-    <>
+    <CartContextProvider>
       <Header />
       <Meals />
-    </>
+    </CartContextProvider>
   );
 }
 
