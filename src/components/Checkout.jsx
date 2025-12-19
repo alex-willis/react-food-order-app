f15a90184d6725916d3a9f14dfe03d91bd88512c
Alex Willis <willisalex@hotmail.co.uk>
2025-12-19 17:02:22 +0000

    Managing Form Status with Form Actions

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useActionState } from "react";
 import Modal from "./UI/Modal";
 import CartContext from "./store/CartContext";
 import { currencyFormatter } from "../util/formatting";
@@ -19,13 +19,10 @@ export default function Checkout() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
 
-  const {
-    data,
-    isLoading: isSending,
-    error,
-    sendRequest,
-    clearData,
-  } = useHttp("https://npkmg8-3000.csb.app/orders", requestConfig);
+  const { data, error, sendRequest, clearData } = useHttp(
+    "https://npkmg8-3000.csb.app/orders",
+    requestConfig
+  );
 
   const cartTotal = cartCtx.items.reduce((totalPrice, item) => {
     return totalPrice + item.quantity * item.price;
@@ -40,7 +37,7 @@ export default function Checkout() {
     clearData();
   }
 
-  async function checkoutAction(fd) {
+  async function checkoutAction(prevState, fd) {
     const customerData = Object.fromEntries(fd.entries());
 
     await sendRequest(
@@ -53,6 +50,8 @@ export default function Checkout() {
     );
   }
 
+  const [formState, formAction, pending] = useActionState(checkoutAction, null);
+
   let actions = (
     <>
       <Button type="button" textOnly onClick={handleClose}>
@@ -62,7 +61,7 @@ export default function Checkout() {
     </>
   );
 
-  if (isSending) {
+  if (pending) {
     actions = <span>Sending order data...</span>;
   }
 
@@ -86,7 +85,7 @@ export default function Checkout() {
   }
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form action={checkoutAction}>
+      <form action={formAction}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
