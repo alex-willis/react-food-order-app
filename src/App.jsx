529cc06e620f29c3b21a9c6bb6647e2ac644d0fb
Alex Willis <willisalex@hotmail.co.uk>
2025-12-19 11:39:14 +0000

    Merge changes from branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useEffect } from "react";
 import Header from "./components/Header";
 import Meals from "./components/Meals";
+import Cart from "./components/Cart";
+import { CartContextProvider } from "./components/store/CartContext";
+import { UserProgressContextProvider } from "./components/store/UserProgressContext";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Meals />
-    </>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+        <Cart />
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
